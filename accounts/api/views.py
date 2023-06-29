74ce30b89b7627de07003792c0f6fde5a8b8a789
Vvn <awayleung2016@gmail.com>
2023-06-28 19:19:48 -0700

    login and logout

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -1,13 +1,31 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework import permissions
+from rest_framework.response import Response
 from accounts.api.serializers import UserSerializer
+from django.contrib.auth import logout as django_logout
 
 
-class UserViewSet(viewsets.ModelViewSet):
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+class AccountViewSet(viewsets.ViewSet):
+    serializer_class = UserSerializer
+
+    @action(methods=['GET'], detail=False)
+    def login_status(self, request):
+        data = {'has_logged_in': request.user.is_authenticated}
+        if request.user.is_authenticated:
+            data['user'] = UserSerializer(request.user).data
+        return Response(data)
+    
+    @action(methods=['POST'], detail=False)
+    def logout(self, request):
+        django_logout(request)
+        return Response({'success': True})
